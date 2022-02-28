3208f7741ffe60347c11c7151878d69648ca0c7f
dpgrrant <80363299+dpgrrant@users.noreply.github.com>
2022-02-28 18:26:18 -0500

    .

--- elevator.c
+++ elevator.c
@@ -10,7 +10,7 @@
 #include <linux/random.h>
 #include <linux/kthread.h>
 
-#define ENTRY_NAME "animal_list"
+#define ENTRY_NAME "Elevator"
 #define PERMS 0644
 #define PARENT NULL
 static struct file_operations fops;
@@ -20,13 +20,12 @@ struct thread_parameter{
     struct mutex my_mutex;
     struct task_struct *kthread;
     int id;
-    int c_state;
+    int c_state;                //c_ == current_ n_ == next_
     int n_state;
     int c_floor;
     int n_floor;
     int c_weight;
     int c_occupants;
-
 }
 #define MAX_PETS 10
 #define MAX_WEIGHT 100
@@ -39,4 +38,4 @@ typedef struct{
     int pet_type;
     int boarding_floor;
     int destination_floor;
-}Pets;
+}Pet;
