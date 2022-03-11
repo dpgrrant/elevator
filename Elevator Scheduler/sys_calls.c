325fd380415ffec44bab34d51a18bf2455c41878
anjaliag <anjaliagrawal590@gmail.com>
2022-03-11 14:30:31 -0500

    Update Sys_call file to add stubs

---
+++ Elevator Scheduler/sys_calls.c
@@ -0,0 +1,30 @@
+#include <linux/linkage.h>
+#include <linux/kernel.h>
+#include <linux/module.h>
+
+int (*STUB_issue_request)(int,int,int) = NULL;
+EXPORT_SYMBOL(STUB_issue_request);
+asmlinkage int sys_issue_request(int pet_type, int current_floor, int final_floor) {
+	if (STUB_issue_request)
+		return STUB_issue_request(pet_type, current_floor, final_floor);
+	else
+		return -ENOSYS;
+}
+
+int (*STUB_start_elevator)(void) = NULL;
+EXPORT_SYMBOL(STUB_start_elevator);
+asmlinkage int sys_start_elevator(void) {
+	if (STUB_start_elevator)
+		return STUB_start_elevator();
+	else
+		return -ENOSYS;
+}
+
+int (*STUB_stop_elevator)(void) = NULL;
+EXPORT_SYMBOL(STUB_stop_elevator);
+asmlinkage int sys_stop_elevator(void) {
+	if (STUB_stop_elevator)
+		return STUB_stop_elevator();
+	else
+		return -ENOSYS;
+}
